74ab5c6953f8beb2012f8aced31b1616030804e2
HilmiGH <99980714+HilmiGH@users.noreply.github.com>
2022-02-25 16:46:33 +0700

    Add files via upload

--- ProcessAName5026211152.java
+++ ProcessAName5026211152.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class ProcessAName5026211152 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your full name (Just 2 Word) : ");
+        System.out.print("\nPlease enter your full name (Just 2 Word) : ");
         String name = input.nextLine();
         int spaceDivider = name.indexOf(" ");
         String firstName = name.substring(0, 1) + ".";
         String lastName = name.substring(spaceDivider + 1);
         String finalName = lastName + ", " + firstName;
+        System.out.println(spaceDivider);
         System.out.print("Your name is : " + finalName);
 
 
